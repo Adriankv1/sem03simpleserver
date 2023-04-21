ca05ae13b240eb8725c5c7953a63e030dde0146a
Adriankv1 <Adriankv@uia.no>
2023-04-21 14:16:30 +0000

    update

--- tcp_server.go
+++ tcp_server.go
@@ -1,56 +1,91 @@
 package main
 
 import (
-	"io"
-	"log"
-	"net"
-	"sync"
+    "fmt"
+    "io"
+    "log"
+    "net"
+    "strconv"
+    "strings"
+    "sync"
+
+    "github.com/Adriankv1/is105sem03/mycrypt"
+    //"github.com/Adriankv1/misc/conv"
 )
 
+func CelsiusToFahrenheit(value float64) float64 { // Celsius to Fahrenheit
+	return (value * 9 / 5) + 32
+}
+
 func main() {
+    var wg sync.WaitGroup
 
-	var wg sync.WaitGroup
+    server, err := net.Listen("tcp", "172.17.0.3:")
+    if err != nil {
+        log.Fatal(err)
+    }
+    defer server.Close()
 
-	server, err := net.Listen("tcp", "127.0.0.1:")
-	if err != nil {
-		log.Fatal(err)
-	}
-	log.Printf("bundet til %s", server.Addr().String())
-	wg.Add(1)
-	go func() {
-		defer wg.Done()
-		for {
-			log.Println("før server.Accept() kallet")
-			conn, err := server.Accept()
-			if err != nil {
-				return
-			}
-			go func(c net.Conn) {
-				defer c.Close()
-				for {
-					buf := make([]byte, 1024)
-					n, err := c.Read(buf)
-					if err != nil {
-						if err != io.EOF {
-							log.Println(err)
-						}
-						return // fra for løkke
-					}
-					switch msg := string(buf[:n]); msg {
-  				        case "ping":
-						_, err = c.Write([]byte("pong"))
-					default:
-						_, err = c.Write(buf[:n])
-					}
-					if err != nil {
-						if err != io.EOF {
-							log.Println(err)
-						}
-						return // fra for løkke
-					}
-				}
-			}(conn)
-		}
-	}()
-	wg.Wait()
+    log.Printf("bundet til %s", server.Addr().String())
+
+    wg.Add(1)
+    go func() {
+        defer wg.Done()
+
+        for {
+            log.Println("før server.Accept() kallet")
+
+            conn, err := server.Accept()
+            if err != nil {
+                log.Println(err)
+                continue
+            }
+
+            wg.Add(1)
+            go func(c net.Conn) {
+                defer wg.Done()
+                defer conn.Close()
+
+                for {
+                    buf := make([]byte, 1024)
+                    n, err := c.Read(buf)
+                    if err != nil {
+                        if err != io.EOF {
+                            log.Println(err)
+                        }
+                        return
+                    }
+                    dekryptertMelding := mycrypt.Krypter([]rune(string(buf[:n])), mycrypt.ALF_SEM03, len(mycrypt.ALF_SEM03)-4)
+                    log.Println("Dekrypter melding: ", string(dekryptertMelding))
+
+                    msgString := string(dekryptertMelding)
+
+                    switch msgString {
+                    case "ping":
+                        kryptertMelding := mycrypt.Krypter([]rune("pong"), mycrypt.ALF_SEM03, 4)
+                        log.Println("Kryptert melding: ", string(kryptertMelding))
+                        _, err = c.Write([]byte(string(kryptertMelding)))
+                    case "Kjevik":
+                        parts := strings.Split(msgString, " ")
+                        celsius, _ := strconv.ParseFloat(parts[1], 64)
+                        fahrenheit := CelsiusToFarenheit(celsius)
+                        kryptertMelding := mycrypt.Krypter([]rune(fmt.Sprintf("Temperaturen i Fahrenheit er %.2f", fahrenheit)), mycrypt.ALF_SEM03, len(mycrypt.ALF_SEM03)-4)
+                        log.Println("Kryptert melding: ", string(kryptertMelding))
+                        _, err = c.Write([]byte(string(kryptertMelding)))
+                    default:
+                        _, err = c.Write(buf[:n])
+                    }
+
+                    if err != nil {
+                        if err != io.EOF {
+                            log.Println(err)
+                        }
+                        return
+                    }
+                }
+            }(conn)
+        }
+    }()
+
+    wg.Wait()
 }
